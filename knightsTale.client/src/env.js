66f927a7fa40e1bb373f148e6f8e0e2e5a11575c
Zach <z6chy@icloud.com>
2022-02-23 13:46:49 -0700

    init

--- knightsTale.client/src/env.js
+++ knightsTale.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-9kkxugcf.us.auth0.com'
+export const clientId = 'GPrpTPaUi1n5T9onzs7W9EUeVqVhwT6m'
+export const audience = 'https://gregslist.com'
